216eb2adcffd3553650181511e06084e3db483fd
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-03-28 00:30:19 +0300

    YouTube scroll assignment done

--- src/_17_07_11_2022_Selenium_Wait_Scroll_Odevleri_Robot_Class/Q2.java
+++ src/_17_07_11_2022_Selenium_Wait_Scroll_Odevleri_Robot_Class/Q2.java
@@ -1,7 +1,15 @@
 package _17_07_11_2022_Selenium_Wait_Scroll_Odevleri_Robot_Class;
 
 import Utility.BaseDriverEdge;
+import Utility.MyFunc;
 import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Q2 extends BaseDriverEdge {
     /*
@@ -15,7 +23,27 @@ public class Q2 extends BaseDriverEdge {
     @Test
     public void Test(){
         driver.get("https://www.youtube.com/");
+        driver.findElement(By.xpath("(//*[@class='yt-spec-touch-feedback-shape__fill'])[12]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        MyFunc.Wait(6);
+        WebElement searchButton = driver.findElement(By.cssSelector("input[id='search']"));
+        searchButton.sendKeys("selenium");
+        MyFunc.Wait(1);
+        searchButton.sendKeys(Keys.ENTER);
+        //MyFunc.Wait(3);
 
+        MyFunc.Wait(0.6);
+
+        List<WebElement> videos;
+        do {
+            videos = driver.findElements(By.cssSelector("h3[class*='style-scope']>[id='video-title']"));
+            //WebElement theVideo = videos.get(videos.size()-1);
+            //new Actions(driver).scrollToElement(theVideo).build().perform();
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollBy(0, 3100);");
+        }while (videos.size()<80);
+
+        System.out.println("Last video title = " + videos.get((videos.size())-1).getText());
         driverStop();
     }
 }
